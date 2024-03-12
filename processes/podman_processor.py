97262ca9629ada38e8aeb8c976283cb89224c8cd
elnaz.azmi <elnaz.azmi@kit.edu>
2024-03-12 18:25:38 +0100

    update pull in podman processor!

--- processes/podman_processor.py
+++ processes/podman_processor.py
@@ -19,11 +19,15 @@ class PodmanProcessor():
 
     def pull_run_image(client, image_name, container_name, environment=None, mounts=None, network_mode=None, volumes=None, command=None):
         # Pull the Docker image
-        for image in client.images.list():
-            print('image list: ', image, image.id, "\n")
-            if image_name not in image.labels['org.opencontainers.image.title']:
-                print(f"Pulling Podman image: {image_name}")
-                image = client.images.pull(image_name)
+        if client.images.list():
+            for image in client.images.list():
+                print('image list: ', image, image.id, "\n")
+                if image_name not in image.labels['org.opencontainers.image.title']:
+                    print(f"Pulling Podman image: {image_name}")
+                    image = client.images.pull(image_name)
+        else:
+            print(f"Pulling Podman image: {image_name}")
+            image = client.images.pull(image_name)
 
         # Check if container with the same name exists, and remove it if it does
         if container_name in client.containers.list():
