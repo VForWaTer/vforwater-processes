35e2d76917711c71cf3b93d58d9618cf9ee3bbd7
MarcusStrobl <marcus.strobl@kit.edu>
2024-03-15 10:16:55 +0100

    Add some Copyright info

--- processes/tool_vforwater_loader.py
+++ processes/tool_vforwater_loader.py
@@ -1,15 +1,41 @@
+# =================================================================
+#
+# Authors: Marcus Strobl <marcus.strobl@kit.edu>
+#
+# Copyright (c) 2024 Marcus Strobl
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
+
 import logging
 import json
 
 from pygeoapi.process.base import BaseProcessor, ProcessorExecuteError
-import os
+
 # from toolbox_runner import list_tools
 from toolbox_runner.run import get_remote_image_list
 
 from processes.podman_processor import PodmanProcessor
-
-# LOGGER = logging.getLogger(__name__)
-# logging.basicConfig(filename='vforwater_loader.log', encoding='utf-8', level=logging.DEBUG)
 
 #: Process metadata and description
 PROCESS_METADATA = {
